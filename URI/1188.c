4380e881e5ef7f267cbf35d329e05805b1484a30
Sandra Bastos <sandra@vermillion.net.br>
2019-04-04 23:29:31 -0300

    added 1188.c

---
+++ URI/1188.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define n 12
+
+int main()
+{
+	int rows, columns;
+	int count = 0;
+	char operation;
+	double sum = 0.0;
+	double matrix[n][n];
+
+	scanf("%c ", &operation);
+
+	for (rows = 0; rows < n; rows++){
+		for (columns = 0; columns < n; columns++){
+			scanf("%lf", &matrix[rows][columns]);
+
+			if (rows > columns && (columns + rows) >= n){
+				sum += matrix[rows][columns];
+				count++;
+			}
+		}
+	}
+
+	if (operation == 'M'){
+		printf("%.1lf\n", sum/count);
+
+	} else if (operation == 'S'){
+		printf("%.1lf\n", sum);
+	}
+
+	return 0;
+}
